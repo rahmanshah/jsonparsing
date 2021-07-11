8134916041a1c6cc3a44ce66b2b1e0540cf474e2
Shah rahman <rahman.shah@protonmail.ch>
2021-07-11 11:43:34 +0300

    parsing content from json file - modified

--- second.py
+++ second.py
@@ -2,3 +2,9 @@ import json
 
 #here we will try to parse content from json file
 
+with open('courses.json') as f:
+    dict_file = json.load(f)
+
+    print(type(dict_file))
+
+    print(dict_file)
